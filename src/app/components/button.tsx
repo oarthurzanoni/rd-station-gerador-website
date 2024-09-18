290f16d7341cc6256d7a73ee1ca45cf6f669560d
Arthur <oarthurzanoni@gmail.com>
2024-09-18 12:07:23 -0300

    style: enhance button shadow

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -6,12 +6,12 @@ const buttonRootClassName = tv({
 });
 
 const buttonClassName = tv({
-  base: "flex items-center justify-center gap-2 text-base transition-colors font-bold py-2 px-8 uppercase",
+  base: "button-shadow flex items-center justify-center gap-2 text-base transition-colors font-bold py-2 px-4 uppercase",
   variants: {
     color: {
       primary:
-        "group-disabled:border-rd-gray-200 group-disabled:bg-rd-gray-200 group-disabled:text-rd-gray-600 bg-rd-secondary-500 border border-black text-black group-hover:bg-rd-secondary-400 group-hover:border-rd-secondary-400 group-disabled:border-none",
-      transparent: "text-white",
+        "group-disabled:button-shadow-disabled group-disabled:bg-rd-gray-200 group-disabled:text-rd-gray-600 bg-rd-secondary-500 border border-black text-black group-hover:bg-rd-secondary-400 group-hover:border-rd-secondary-400 group-disabled:border-none",
+      transparent: "text-white button-shadow-transparent",
     },
   },
   defaultVariants: {
@@ -41,12 +41,6 @@ export function Button({
         <span>{text}</span>
         {iconRight}
       </div>
-      {color !== "transparent" ? (
-        <span
-          className="absolute bottom-[-6px] left-0 w-full h-[6px] bg-black group-disabled:bg-rd-gray-600 z-10 transition-all"
-          style={{ clipPath: "polygon(0 0, 100% 0, 98% 100%, 2% 100%)" }}
-        />
-      ) : null}
     </button>
   );
 }
